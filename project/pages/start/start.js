baf91c200611bd2fe75571f4287dafcec0757481
二狗子 <1044154220@qq.com>
2018-07-24 14:30:46 +0800

    添加了身份验证
    添加了密码认证及指纹验证

--- project/pages/start/start.js
+++ project/pages/start/start.js
@@ -1,4 +1,6 @@
 // pages/start/start.js
+const utils = require('../../utils/util.js')
+const sha1 = require('../../utils/sha1.js')
 Page({
 
   /**
@@ -16,14 +18,24 @@ Page({
      * 用于记录是否支持指纹验证以及记录是否录入指纹
      */
     isContainFinger:false,
-    isStoareFinger:false
+    isStoareFinger:false,
+    /**
+     * 用于记录是否已经注册了
+     */
+    isLogin: false,
+    /**
+     * 记录密码及再次输入的密码
+     */
+    pass:'',
+    againpass:'',
+    inputLoginPass:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  // 1111
+  
   },
 
   /**
@@ -36,34 +48,85 @@ Page({
         scrollHieght: scrollHieght
       })
   },
+  /**
+   * 输入密码
+   */
+  inputpass:function(event){
+    this.setData({
+      pass:event.detail.value
+    })
+  },
 
   /**
-   * 
+   * 再次输入密码
    */
+  againinputpass:function(event){
+    this.setData({
+      againpass: event.detail.value
+    })
+  },
 
   /**
    * 进入小程序
    */
   goto:function(){
-    let isselected = this.data.isselected
-
-    if (!isselected){
-
-      wx.showModal({
-        title: '尚未同意使用协议',
-        content: '请认真阅读并同意该协议。',
-        showCancel:false
-      })
-      return 0
-    } 
-
     /**
-     * 跳转小程序
+     * 判断当前是指纹验证还是密码认证，如果是密码认证，则验证2个密码是否一致
      */
 
-    wx.navigateTo({
-      url: '../list/list',
-    })
+    let authWay = this.data.authWay
+
+    let pass = this.data.pass
+    let againpass = this.data.againpass
+    let userid = ''
+
+    if(authWay == 'pass'){
+      if ((utils.isempty(pass) == 0) || (utils.isempty(againpass) == 0)) {
+        utils.gkShowModel('验证失败','密码不能为空')
+        return 0;
+      }
+
+      if (pass != againpass){
+        utils.gkShowModel('验证失败', '两次输入的密码不一致！')
+        return 0;
+      }
+
+      /**
+       * 进行数据加密
+       */
+      let sha1pass = sha1.hex_sha1(pass)
+      wx.setStorageSync('pass', sha1pass)
+      wx.setStorageSync('isLogin', true)
+      wx.setStorageSync('authWay', authWay)
+
+      utils.showsuccess('验证成功!')
+      setTimeout(function () {
+        /**
+         * 跳转小程序
+         */
+        wx.redirectTo({
+          url: '../list/list',
+        })
+      }, 1000)
+
+    }else{
+
+      let isContainFinger = this.data.isContainFinger
+      let isStoareFinger = this.data.isStoareFinger
+      if(isContainFinger == false){
+        utils.gkShowModel('验证失败', '小程序暂未支持当前手机类型的指纹验证，请更换成密码验证！')
+        return 0;
+      }
+
+      if (isStoareFinger == false) {
+        utils.gkShowModel('验证失败', '检测到您的手机未录入相关指纹，请更换成密码验证！')
+        return 0;
+      }
+
+
+      this.checkfingtap()
+    }
+    
   },
 
   /**
@@ -82,6 +145,10 @@ Page({
   onShow: function () {
     this.getAuthens()
     this.isStoareAuthen()
+    // 判断是否已经注册登录过了
+    this.checkLogin()
+    // 判断之前验证的登录方式
+    this.checkAuthWay()
   },
 
   /**
@@ -145,6 +212,32 @@ Page({
   },
 
   /**
+   * 用于记录是否已经注册了
+   */
+  checkLogin:function(){
+    let islogin = wx.getStorageSync('isLogin')
+    if (islogin == ''){
+      islogin = false
+    }
+
+    this.setData({
+      isLogin:islogin
+    })
+  },
+  /**
+   * 用于记录之前的注册登录方式
+   */
+  checkAuthWay:function(){
+    let authWay = wx.getStorageSync('authWay')
+    if(authWay == ''){
+      authWay = 'finger'
+    }
+    this.setData({
+      authWay:authWay
+    })
+  },
+
+  /**
    * 指纹验证
    */
 
@@ -155,8 +248,71 @@ Page({
         authContent:'请验证身份',
         success:res=>{
           console.log(res)
+          if(res.errCode == 0){
+
+            wx.setStorageSync('isLogin', true)
+            wx.setStorageSync('authWay', 'finger')
+            utils.showsuccess('验证成功!')
+            setTimeout(function(){
+              /**
+               * 跳转小程序
+               */
+              wx.redirectTo({
+                url: '../list/list',
+              })
+            },1000)
+          }
         }
       })
+  },
+
+  // 指纹登录
+  fingerLogin:function(){
+    wx.startSoterAuthentication({
+      requestAuthModes: ['fingerPrint'],
+      challenge: '123457688',
+      authContent: '请验证身份',
+      success: res => {
+        if (res.errCode == 0) {
+          utils.showsuccess('验证成功!')
+          setTimeout(function () {
+            /**
+             * 跳转小程序
+             */
+            wx.redirectTo({
+              url: '../list/list',
+            })
+          }, 1000)
+        }
+      }
+    })
+  },
+
+  inputLogin:function(){
+    let sha1pass = wx.getStorageSync('pass')
+    let authPass = this.data.inputLoginPass
+
+    let sha1AuthPass = sha1.hex_sha1(authPass)
+
+    if (sha1pass == sha1AuthPass){
+      utils.showsuccess('验证成功!')
+      setTimeout(function () {
+        /**
+         * 跳转小程序
+         */
+        wx.redirectTo({
+          url: '../list/list',
+        })
+      }, 1000)
+    }else{
+      utils.gkShowModel('登录失败','请检查密码！')
+    }
+
+  },
+  inputLoginText:function(event){
+    this.setData({
+      inputLoginPass:event.detail.value
+    })
   },
 
   /**
--- project/utils/util.js
+++ project/utils/util.js
@@ -5,8 +5,98 @@ const formatTime = date => {
   const hour = date.getHours()
   const minute = date.getMinutes()
   const second = date.getSeconds()
-
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+// 将数字转换为数组
+function convertToStarsArray(stars) {
+  var num = stars / 2;
+  var array = [];
+  for (var i = 1; i <= 5; i++) {
+    if (i <= num) {
+      array.push(1);
+    }
+    else {
+      if ((i - num) === 0.5) {
+        array.push(0.5)
+      }
+      else {
+        array.push(0);
+      }
+    }
+  }
+  return array;
+}
+
+// 判断字符串是否为空，用于在POST请求前对输入框输入的内容就行检查
+function isempty(str) {
+  var newstr = str.replace("/(^\s*)|(\s*$)/g", "")
+
+  return newstr.length
+}
+function showsuccess(str) {
+  wx.showToast({
+    title: str,
+    icon: 'success',
+    duration:1000
+  })
+}
+
+// 请求数据,需传入完整的url
+function getData(url, dosomething) {
+  wx.request({
+    url: url,
+    data: [],
+    success: function (res) {
+      dosomething(res)
+    }
+  })
+}
+
+/**
+ * POST请求，用于获取数据或提交数据
+ */
+// application/json默认值
+function post(posturl, params, dosomething, complete) {
+
+  wx.request({
+    url: posturl,
+    data: params,
+    header: {
+      'content-type': 'application/json'
+    },
+    method: 'POST',
+    success: function (res) {
+      dosomething(res)
+    },
+    fail: function (err) {
+      myshowmodel('请求失败', err.errMsg)
+      return 0
+    },
+    complete: function () {
+      complete()
+    }
+  })
+}
+
+// 加载提示
+function setloading() {
+  wx.showLoading({
+    title: '玩命加载中...',
+  })
+}
+
+// 隐藏加载提示
+function hidemyloading() {
+  wx.hideLoading()
+}
+
+// 提示框
+function gkShowModel(title, content) {
+  wx.showModal({
+    title: title,
+    content: content,
+    showCancel: false,
+  })
 }
 
 const formatNumber = n => {
@@ -15,5 +105,13 @@ const formatNumber = n => {
 }
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  convertToStarsArray: convertToStarsArray,
+  getData: getData,
+  gkShowModel: gkShowModel,
+  isempty: isempty,
+  post: post,
+  setloading: setloading,
+  hidemyloading: hidemyloading,
+  showsuccess: showsuccess
 }
